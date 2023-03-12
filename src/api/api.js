aaab2518df55df76a8f3c75ace5a7092c954cfd4
teetawat <teetawat.tch@gmail.com>
2023-03-12 20:21:39 +0700

    chage ip api to url https

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ let url = "";
 if (process.env.NODE_ENV === "development") {
   url = "http://127.0.0.1:8000/";
 } else {
-  url = "http://139.59.126.166/";
+  url = "https://www.tee-dev.online/";
 }
 
 export default axios.create({
