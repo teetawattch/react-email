da990b6196fd725ad83952e46f19c0fe26f0529b
teetawat <teetawat.tch@gmail.com>
2023-03-12 18:20:01 +0700

    set api url

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ let url = "";
 if (process.env.NODE_ENV === "development") {
   url = "http://127.0.0.1:8000/";
 } else {
-  url = "http://0.0.0.0/";
+  url = "http://139.59.126.166/";
 }
 
 export default axios.create({
